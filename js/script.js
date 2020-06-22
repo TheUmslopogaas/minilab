5727bd40078df7c250fcbd8d8f99c5088e7ca495
Nazmulhasan Kazal <nazmulhasankazal@gmail.com>
2020-06-22 22:28:29 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Owl Carousel
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:20,
+  nav:false,
+  center:false,
+  responsive:{
+    0:{
+        items:1.5
+    },
+    992:{
+        items:2.5
+    }
+  }
+})
